0a573d1054ce23317491be4e44b76ba9416145df
AllebrantJulia <anajuliaallebrant5@gmail.com>
2026-05-20 01:09:30 -0300

    Implementando listagem de produtos cadastrados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -46,7 +46,27 @@ public class ProdutosDAO {
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
+        String sql = "SELECT * FROM produtos";
         
+        conn = new conectaDAO().connectDB();
+        
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                
+                listagem.add(produto);
+                
+            }
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + erro.getMessage());
+        }
         return listagem;
     }
     
